7a8da084a8cedf8213726100cfbf154ea461f26a
Arpit Khandelwal <masterarpit@gmail.com>
2025-09-05 12:29:12 +0530

    update

--- league/apps.py
+++ league/apps.py
@@ -1,13 +1,10 @@
 from django.apps import AppConfig
 
+
 class LeagueConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "league"
 
     def ready(self):
         # Import models here to avoid circular imports during app loading
-        # from .models import initialize_league_data
         import league.signals
-        # This will run once when the Django app starts
-        # initialize_league_data()
-        
