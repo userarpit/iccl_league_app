8d5fba9584f9dadd1abebaaad7184f35f9abb824
Arpit Khandelwal <masterarpit@gmail.com>
2025-09-25 08:54:47 +0530

    update setting

--- iccl_league_app/settings.py
+++ iccl_league_app/settings.py
@@ -187,3 +187,18 @@ EMAIL_HOST_PASSWORD = (
 )
 
 CSRF_TRUSTED_ORIGINS = ["https://phonotypically-unchanneled-sommer.ngrok-free.app"]
+
+# settings.py
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
