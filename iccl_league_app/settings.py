f07e9567c2608ba2b430343a2cfa4e69d24023bd
Arpit Khandelwal <masterarpit@gmail.com>
2025-09-30 19:55:21 +0530

    update

--- iccl_league_app/settings.py
+++ iccl_league_app/settings.py
@@ -132,6 +132,7 @@ DATABASES = {
         "PASSWORD": os.getenv("DATABASE_PASSWORD"),
         "HOST": os.getenv("DATABASE_HOST"),
         "PORT": os.getenv("DATABASE_PORT"),
+        "OPTIONS": {"sslmode": "require"}
     }
 }
 
